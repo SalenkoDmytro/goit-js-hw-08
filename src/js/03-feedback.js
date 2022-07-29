d7799e498cb4c24edcec9ecca8c9b30ffd2c0ee2
SalenkoDmytro <dmytrosalenko1996@gmail.com>
2022-07-29 18:52:19 +0300

    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,43 +1,43 @@
 var throttle = require('lodash.throttle');
 
+const FORM_INPUTS = 'feedback-form-state';
 const refs = {
   form: document.querySelector('.feedback-form'),
   input: document.querySelector('.feedback-form input'),
   textarea: document.querySelector('.feedback-form textarea'),
-  button: document.querySelector('.feedback-form button'),
 };
-const STORAGE_KEY = 'message';
+const formData = {};
+
+populateInput(getFormData());
+populateEmail(getFormData());
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.input.addEventListener('input', throttle(onInputType, 500));
-refs.textarea.addEventListener('input', throttle(onTextareaType, 500));
-
-populateTextarea();
-// const formInputs = {
-//   email: '',
-//   message: '',
-// };
+refs.form.addEventListener('input', throttle(onFormInput, 500));
 
 function onFormSubmit(evt) {
   evt.preventDefault();
 
   evt.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
+  localStorage.removeItem(FORM_INPUTS);
 }
 
-function onInputType(evt) {
-  // formInputs.email = evt.currentTarget.value;
-}
-function onTextareaType(evt) {
-  // formInputs.message = evt.currentTarget.value;
-  localStorage.setItem(STORAGE_KEY, evt.target.value);
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(FORM_INPUTS, JSON.stringify(formData));
 }
 
-function populateTextarea() {
-  const savedMessage = localStorage.getItem(STORAGE_KEY);
-  if (savedMessage) {
-    refs.textarea.value = savedMessage;
+function populateEmail(data) {
+  data.email ? (refs.input.value = data.email) : '';
+}
+function populateInput(data) {
+  data.message ? (refs.textarea.value = data.message) : '';
+}
+
+function getFormData() {
+  try {
+    return JSON.parse(localStorage.getItem(FORM_INPUTS));
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
   }
 }
-// localStorage.setItem('feedback-form-state', JSON.stringify(formInputs));
-// console.log(formInputs);
