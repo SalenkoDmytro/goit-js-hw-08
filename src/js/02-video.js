d236efa0c210fb0ff78736f275fc29e46e800699
SalenkoDmytro <dmytrosalenko1996@gmail.com>
2022-07-29 13:00:51 +0300

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,5 +2,14 @@ import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+var throttle = require('lodash.throttle');
 
-player.on('timeupdate', function (data) {});
+const onTimeupdateTrottle = throttle(videoCurrentTime, 1000);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+player.on('timeupdate', onTimeupdateTrottle);
+
+function videoCurrentTime(data) {
+  localStorage.setItem('videoplayer-current-time', Math.floor(data.seconds));
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,21 @@
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+  button: document.querySelector('.feedback-form button'),
+};
 
+refs.form.addEventListener('submit', onFormSubmit);
+refs.input.addEventListener('input', onInputType);
+refs.textarea.addEventListener('input', onTextareaType);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+}
+
+function onInputType(evt) {
+  const message = evt.currentTarget.value;
+}
+function onTextareaType(evt) {
+  const message = evt.currentTarget.value;
+}
